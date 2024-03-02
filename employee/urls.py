472b839a742c8c0568225df0d3f6e52503d67fc9
Shuhei Fukami <shuheifukami@Shuheis-MacBook-Air-M1-private.local>
2024-03-02 21:56:32 +0900

    revise menu button

--- employee/urls.py
+++ employee/urls.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from . import views
 
-app_name = 'employee'
+from . import views
+from .views import EmployeeDetailView, UserLoginView
+
+app_name = "employee"
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path("list/", views.IndexView.as_view(), name="index"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("employee/<str:eid>/", EmployeeDetailView.as_view(), name="detail"),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,16 +1,42 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views import generic
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+
 from .forms import SearchForm
 from .models import Employee
 
 
+class UserLoginView(LoginView):
+    template_name = "login.html"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("list")  # ログイン後にリダイレクトするURL
+
+
+class EmployeeDetailView(DetailView):
+    model = Employee
+    template_name = "employee_detail.html"
+
+    def get_object(self):
+        eid = self.kwargs.get("eid")
+        return Employee.objects.get(eid=eid)
+
+
 class IndexView(generic.ListView):
     model = Employee
-    paginate_by = 1
+    paginate_by = 10
+    template_name = "employee_list.html"
 
     def get_context_data(self):
         """テンプレートへ渡す辞書の作成"""
         context = super().get_context_data()
-        context['form'] = SearchForm(self.request.GET)   # 基の辞書に、formを追加
+        context["form"] = SearchForm(self.request.GET)  # 基の辞書に、formを追加
         return context
 
     def get_queryset(self):
@@ -22,7 +48,7 @@ class IndexView(generic.ListView):
         queryset = super().get_queryset()
 
         # 部署の選択があれば、部署で絞り込み(filter)
-        department = form.cleaned_data['department']
+        department = form.cleaned_data["department"]
         if department:
             queryset = queryset.filter(department=department)
 
