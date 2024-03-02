2c4b4bb8ab5490e76fca42eef44b40a7d464bdf3
Shuhei Fukami <shuheifukami@Shuheis-MacBook-Air-M1-private.local>
2024-03-02 14:54:41 +0900

    revise datamodels for ACN

--- employee/models.py
+++ employee/models.py
@@ -1,36 +1,66 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 
-class Department(models.Model):
-    name = models.CharField('部署名', max_length=20)
-    created_at = models.DateTimeField('日付', default=timezone.now)
-
-    def __str__(self):
-        return self.name
-
-
-class Club(models.Model):
-    name = models.CharField('部活名', max_length=20)
-    created_at = models.DateTimeField('日付', default=timezone.now)
+class DTE(models.Model):
+    name = models.CharField("DTE", max_length=20)
+    created_at = models.DateTimeField("日付", default=timezone.now)
 
     def __str__(self):
         return self.name
 
 
 class Employee(models.Model):
-    first_name = models.CharField('名', max_length=20)
-    last_name = models.CharField('姓', max_length=20)
-    email = models.EmailField('メールアドレス', blank=True)
-    department = models.ForeignKey(
-        Department, verbose_name='部署', on_delete=models.PROTECT,
+    eid = models.CharField("EID", max_length=10, primary_key=True)
+    eno = models.CharField("社員番号", max_length=10)
+    first_name = models.CharField(
+        "名",
+        max_length=20,
     )
-    club = models.ManyToManyField(
-        Club, verbose_name='部活',
+    middle_name = models.CharField("ミドル", max_length=20, blank=True)
+    last_name = models.CharField("姓", max_length=20)
+    full_name = models.CharField(
+        "氏名", max_length=50, default=f"{first_name} {middle_name} {last_name}"
     )
-    created_at = models.DateTimeField('日付', default=timezone.now)
+    email = models.EmailField("e-mail", blank=True, default=f"{eid}@accenture.com")
+    dte = models.ForeignKey(
+        DTE,
+        verbose_name="DTE",
+        on_delete=models.PROTECT,
+    )
+    office = models.CharField("オフィス", max_length=20, blank=True)
+    created_at = models.DateTimeField("登録日", default=timezone.now)
 
     def __str__(self):
-        return '{0} {1} {2}'.format(self.last_name, self.first_name, self.department)
-    
-    
+        return "{0} {1} {2}".format(self.last_name, self.first_name, self.dte)
+
+
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField("プロジェクト名", max_length=50)
+
+    start_date = models.DateTimeField("開始日", default=timezone.now)
+    end_date = models.DateTimeField("終了日", blank=True, null=True)
+    description = models.TextField("プロジェクトの詳細", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Skill(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    level = models.CharField(max_length=10)
+
+
+class Training(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    completion_date = models.DateField()
+
+
+class History(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    completion_date = models.DateField()
