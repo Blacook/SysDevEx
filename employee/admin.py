ddcea8a73b326c5a9d727f6af43b43c9651247a9
Shuhei Fukami <shuheifukami@Shuheis-MacBook-Air-M1-private.local>
2024-03-02 14:55:59 +0900

    migrate datamodel for ACN

--- employee/admin.py
+++ employee/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Department, Employee, Club
 
-admin.site.register(Department)
+from .models import DTE, Employee
+
+admin.site.register(DTE)
 admin.site.register(Employee)
-admin.site.register(Club)
--- employee/forms.py
+++ employee/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from .models import Club, Department
+
+from .models import DTE
 
 
 class SearchForm(forms.Form):
-    club = forms.ModelChoiceField(
-        queryset=Club.objects, label='サークル', required=False)
 
     department = forms.ModelChoiceField(
-        queryset=Department.objects, label='部署', required=False)
+        queryset=DTE.objects, label="部署", required=False
+    )
